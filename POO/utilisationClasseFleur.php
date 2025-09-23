21b8845a6c6633dcc9a359d8d5fa62b9bdb791fa
Gatien <legrand.gatien77@gmail.com>
2025-09-23 17:45:35 +0200

    petit commit

--- POO/utilisationClasseFleur.php
+++ POO/utilisationClasseFleur.php
@@ -1,11 +1,11 @@
 <?php
 $Alstroemeria_jaune = new Fleur();
 $Alstroemeria_jaune->nom = "Alstroemeria jaune";
-var_dump($Alstroemeria_jaune);
+//var_dump($Alstroemeria_jaune);
 
 $Alstroemeria_violet = new Fleur();
 $Alstroemeria_violet->nom = "Alstroemeria violet";
-var_dump($Alstroemeria_violet);
+//var_dump($Alstroemeria_violet);
 
 echo "Les fleurs sont : " . $Alstroemeria_jaune->nom . " et " . $Alstroemeria_violet->nom;
 ?>
